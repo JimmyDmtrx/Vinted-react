37ffbde7311363cc684ddcc4ce4c755f6c55fe6f
Jimmy Demortreux <jimmydemortreux@mbp-de-jimmy.lan>
2022-02-09 15:17:52 +0100

    annonce

--- src/App.js
+++ src/App.js
@@ -2,24 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
-import axios from "axios";
-import { useState, useEffect } from "react";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState();
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(
-        "https://lereacteur-vinted-api.herokuapp.com/offers"
-      );
-      console.log(response.data);
-      setData(response.data);
-      setIsLoading(false);
-    };
-
-    fetchData();
-  }, []);
   return (
     <Router>
       <Routes>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,32 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offers"
+      );
+      console.log(response.data);
+      setData(response.data);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
   const id = "87654543";
   return (
     <div>
       <Header />
       <Hero />
-      <Link to={`/product/${id}`}>Go to product with Link</Link>
+      <div>{data.offers}</div>
+      <Link to={`/product/`}>Go to product with Link</Link>
     </div>
   );
 };
